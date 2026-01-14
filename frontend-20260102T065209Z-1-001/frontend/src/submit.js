f543464930fba17a2a3e20747a69e8d0e9a01a7a
Ankit Bhagat <ankitbhagat707@gmail.com>
2026-01-14 15:53:43 +0530

    Connect frontend to live backend

--- frontend-20260102T065209Z-1-001/frontend/src/submit.js
+++ frontend-20260102T065209Z-1-001/frontend/src/submit.js
@@ -11,7 +11,7 @@ export const SubmitButton = () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/pipelines/parse", {
+      const response = await fetch("https://vectorshift-dag-assignment.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
